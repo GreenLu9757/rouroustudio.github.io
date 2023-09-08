944b8c5ea20ab9e6453e7ab93eaef28589cd346a
GreenLu9757 <136168532+GreenLu9757@users.noreply.github.com>
2023-09-08 16:45:08 +0800

    almost dead

--- rouStudio/common.js
+++ rouStudio/common.js
@@ -26,5 +26,80 @@ $(function () {
         return false;
     })
 
-   
+    $('.menu-colBar .dropdown-toggle').on('click', function moveToService() {
+        var targetHeight = $('.slide').get(0).scrollHeight;
+        console.log(targetHeight)
+        $(".main").animate({ scrollTop: targetHeight }, 400);
+        return false;
+    })
+    $('.service-text1').on('click', function moveToServiceItem1() {
+        console.log('check1')
+        moveTosServiceItem(1)
+    })
+    $('.service-text2').on('click', function moveToServiceItem2() {
+        console.log('check2')
+        moveTosServiceItem(2)
+    })
+    $('.service-text3').on('click', function moveToServiceItem3() {
+        console.log('check3')
+        moveTosServiceItem(3)
+    })
+    $('.service-text4').on('click', function moveToServiceItem4() {
+        console.log('check4')
+        moveTosServiceItem(4)
+    })
+    $('.service-text5').on('click', function moveToServiceItem5() {
+        console.log('check5')
+        moveTosServiceItem(5)
+        // $('.main').scrollTop('.service-item5')
+    })
+
+    $('.menu-colBar>ul>li:nth-of-type(2)').on('click', function moveToAbout() {
+        console.log('按我~')
+        var targetHeight =$('.slide').get(0).scrollHeight + $('.service').get(0).scrollHeight;
+        console.log(targetHeight)
+        $(".main").animate({ scrollTop: targetHeight }, 400);
+        return false;
+    })
+    $('.menu-colBar>ul>li:nth-of-type(3)').on('click', function moveToAssignment() {
+        console.log('按我~')
+        var targetHeight = $('.service').get(0).scrollHeight + $('.slide').get(0).scrollHeight + $('.about').get(0).scrollHeight;
+        console.log(targetHeight)
+        $(".main").animate({ scrollTop: targetHeight }, 400);
+        return false;
+    })
+    $('.menu-colBar>ul>li:nth-of-type(4)').on('click', function moveToContact() {
+        console.log('按我~')
+        var targetHeight = $('.service').get(0).scrollHeight + $('.slide').get(0).scrollHeight + $('.about').get(0).scrollHeight + $('.assign').get(0).scrollHeight  ;
+        console.log(targetHeight)
+        $(".main").animate({ scrollTop: targetHeight }, 400);
+        return false;
+    })
+    
+
 })
+
+
+
+function moveTosServiceItem(item) {
+    var mainWidth = $('.main').width();
+    var slideHeight = $('.slide').get(0).scrollHeight;
+    var cardHeight = $('.service-item1').get(0).scrollHeight;
+    // var h2Height= $('h2').get(0).scrollHeight;
+    if (mainWidth <= 768) {
+        targetHeight = slideHeight + cardHeight / 0.9 *(item-1);
+        $(".main").animate({ scrollTop: targetHeight}, 400);
+        
+    }
+    else {
+        if (item < 4) {
+            targetHeight = slideHeight + cardHeight*0.5;
+            $(".main").animate({ scrollTop: targetHeight }, 400);
+        }
+        else{
+            targetHeight = slideHeight + cardHeight*1.5;
+            $(".main").animate({ scrollTop: targetHeight }, 400);
+        }
+
+    }
+}
